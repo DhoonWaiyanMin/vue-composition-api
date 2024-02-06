d67009a0e87964595bb7adbc7e20b3958f952657
DhoonWaiYanMin <dhoon166@gmail.com>
2024-02-06 21:17:15 +0630

    fetch data from firebase

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { computed, reactive, ref ,onMounted , onUnmounted } from 'vue';
+import { db } from "../firebase/config"
 
 let getPosts = ()=>{
     let posts = ref([])
@@ -7,16 +8,29 @@ let getPosts = ()=>{
     let load = async()=>{
       try{
         // crearte new promise for loading 
-        await new Promise((resolve,reject)=>{
+        // await new Promise((resolve,reject)=>{
           // resolve();
-          setTimeout(resolve,2000);
-        })
-        let response = await fetch("http://localhost:3000/posts")
-        if(response.status === 404){
-          throw new Error("URL not found");
-        }
-        let datas = await response.json();
-        posts.value = datas;
+        //   setTimeout(resolve,2000);
+        // })
+        // let response = await fetch("http://localhost:3000/posts")
+        // if(response.status === 404){
+        //   throw new Error("URL not found");
+        // }
+        // let datas = await response.json();
+        // posts.value = datas;
+
+
+        // Data from Firebase 
+        let response = await db.collection("posts").get();
+        // console.log(response.docs);
+        posts.value = response.docs.map((doc)=>{
+          // console.log(doc.data());
+          return {
+            id : doc.id,
+            ...doc.data()
+          }
+        });
+        
       }
       catch(err){
         error.value = err.message
---
+++ src/firebase/config.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import  firebase  from "firebase/app";
+// import "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAPC7MadBYv65bFTX3wmW-zVRKt0JcSDnU",
+//   authDomain: "vue-blog-system-7ca56.firebaseapp.com",
+//   projectId: "vue-blog-system-7ca56",
+//   storageBucket: "vue-blog-system-7ca56.appspot.com",
+//   messagingSenderId: "27380129501",
+//   appId: "1:27380129501:web:028f51e415b72189bb4534"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// db setup 
+// let db = firebase.firestore();
+
+// export {app}
+
+import  firebase  from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPC7MadBYv65bFTX3wmW-zVRKt0JcSDnU",
+  authDomain: "vue-blog-system-7ca56.firebaseapp.com",
+  projectId: "vue-blog-system-7ca56",
+  storageBucket: "vue-blog-system-7ca56.appspot.com",
+  messagingSenderId: "27380129501",
+  appId: "1:27380129501:web:028f51e415b72189bb4534"
+};
+
+// firebase init 
+firebase.initializeApp(firebaseConfig)
+
+let db = firebase.firestore()
+
+export {db}
